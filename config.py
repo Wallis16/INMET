164449943581e1cb56e98ba5fab03432c5a2e246
Diógenes Wallis de França Silva <diogeneswallis@outlook.com>
2019-10-26 13:05:20 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Fabio Rodrigues Jorge
+Email: fabinhojorgenet@gmail.com
+Description: Config file.
+"""
+
+LOGIN = {
+    'url': 'http://www.inmet.gov.br/projetos/rede/pesquisa/inicio.php',
+    'username': 'diogeneswallis@outlook.com',
+    'password': '0rj5xyni',
+}
+
+URL_TEMPLATE = {
+    'HOUR': 'http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt.php?&mRelEstacao={omm_code}'
+            '&btnProcesso=serie&mRelDtInicio={start_date}&mRelDtFim={end_date}&mAtributos=1,1,,,1,1,,1,1,,,1,,,,,',
+    'DAY': 'http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt.php?&mRelEstacao={omm_code}'
+           '&btnProcesso=serie&mRelDtInicio={start_date}&mRelDtFim={end_date}&mAtributos=,,1,1,,,,,,1,1,,1,1,1,1,',
+    'DAYFULL': 'http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt.php?&mRelEstacao={omm_code}'
+               '&btnProcesso=serie&mRelDtInicio={start_date}&mRelDtFim={end_date}'
+               '&mAtributos=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+    'MONTH': 'http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt_mensal.php?&mRelEstacao={omm_code}'
+             '&btnProcesso=serie&mRelDtInicio={start_date}&mRelDtFim={end_date}'
+             '&mAtributos=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+}
